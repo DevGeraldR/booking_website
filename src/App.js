9edd8869ea60a78a795ca6e6e438f5fcb1b15c5a
DevGeraldR <romerogerald22@gmail.com>
2022-12-05 13:39:43 +0800

    Calendar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Welcome from "./pages/Welcome";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BookSchedule from "./components/Book/BookSchedule";
 
 function App() {
-  return <Welcome />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/book_schedule" element={<BookSchedule />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/welcome/Book.jsx
+++ src/components/welcome/Book.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Book() {
-  return <div className="flex h-screen w-full">Book A Schedule</div>;
+  return (
+    <div className="flex bg-[#87ab69]">
+      <div className="flex w-full h-[500px] flex-col justify-between py-20 items-center bg-[#87ab69]">
+        <div className="flex flex-col text-center gap-4">
+          <span className="text-4xl font-bold">Book a Schedule</span>
+          <span className="text-white text-lg">Reserve a date</span>
+        </div>
+        <Link to="/book_schedule">
+          <button className="bg-green-900 p-3">BOOK A SCHEDULE</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Book;
